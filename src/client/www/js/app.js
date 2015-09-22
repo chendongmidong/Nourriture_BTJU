f3cd4e1f12c2935be650496361d522c1714ed6d3
Alexy <alexy.hody@epitech.eu>
2015-09-22 21:34:45 +0800

    +client start ionic

---
+++ src/client/www/js/app.js
@@ -0,0 +1,107 @@
+// Ionic Nourriture App
+
+angular.module('nourriture', ['ionic', 'nourriture.controllers', 'nourriture.services', 'ngResource'])
+
+.run(function ($ionicPlatform, $window, $http) {
+
+    $window.localStorage.apiUrl = 'http://tossabox.com:8080/api/';
+
+    $http.defaults.headers.common['Access-Control-Allow-Origin'] = "*";
+    $http.defaults.headers.common['Access-Control-Allow-Methods'] = "GET, POST, PUT, DELETE, OPTIONS";
+    $http.defaults.headers.common['Access-Control-Allow-Headers'] = "Authorization";
+
+
+    $ionicPlatform.ready(function () {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            cordova.plugins.Keyboard.disableScroll(true);
+
+        }
+        if (window.StatusBar) {
+            // org.apache.cordova.statusbar required
+            StatusBar.styleLightContent();
+        }
+    });
+})
+
+.config(function ($stateProvider, $urlRouterProvider, $httpProvider, $interpolateProvider, $resourceProvider) {
+
+
+
+
+    // Ionic uses AngularUI Router which uses the concept of states
+    // Learn more here: https://github.com/angular-ui/ui-router
+    // Set up the various states which the app can be in.
+    // Each state's controller can be found in controllers.js
+
+
+    /* Python Django config */
+
+    // Change tag symbols, replace {{ }} with [[ ]]
+    //    $interpolateProvider.startSymbol('[[').endSymbol(']]');
+
+    // Add CSRF Token support
+    $httpProvider.defaults.xsrfCookieName = 'csrftoken';
+    $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
+
+    // Don&rsquo;t strip trailing slashes
+    $resourceProvider.defaults.stripTrailingSlashes = false;
+
+    /* Python Django config */
+
+    $stateProvider
+
+    // setup an abstract state for the tabs directive
+        .state('tab', {
+        url: '/tab',
+        abstract: true,
+        templateUrl: 'templates/tabs.html'
+    })
+
+    // Each tab has its own nav history stack:
+
+    .state('tab.dash', {
+        url: '/dash',
+        views: {
+            'tab-dash': {
+                templateUrl: 'templates/tab-dash.html',
+                controller: 'DashCtrl'
+            }
+        }
+    })
+
+    .state('tab.chats', {
+            url: '/chats',
+            views: {
+                'tab-chats': {
+                    templateUrl: 'templates/tab-chats.html',
+                    controller: 'ChatsCtrl'
+                }
+            }
+        })
+        .state('tab.chat-detail', {
+            url: '/chats/:chatId',
+            views: {
+                'tab-chats': {
+                    templateUrl: 'templates/chat-detail.html',
+                    controller: 'ChatDetailCtrl'
+                }
+            }
+        })
+
+    .state('tab.account', {
+        url: '/account',
+        views: {
+            'tab-account': {
+                templateUrl: 'templates/tab-account.html',
+                controller: 'AccountCtrl'
+            }
+        }
+    });
+
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/tab/dash');
+
+});
